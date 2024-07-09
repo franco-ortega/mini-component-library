923e7cebb554b557827538d8560ab95be0c31ae4
Franco Ortega <francopdx@gmail.com>
2024-07-09 16:25:36 -0700

    increase border radius for large

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,21 +5,27 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const SIZES = {
+const STYLES = {
 	large: {
 		'--height': `${24 / 16}rem`,
 		'--padding': '4px',
+		'--borderRadius': `${8 + 4}px`,
 	},
 	medium: {
 		'--height': `${12 / 16}rem`,
+		'--borderRadius': '4px',
 	},
 	small: {
 		'--height': `${8 / 16}rem`,
+		'--borderRadius': '4px',
 	},
 };
 
 const ProgressBar = ({ value, size }) => {
-	const styles = SIZES[size];
+	const styles = STYLES[size];
+
+	if (!styles)
+		throw new Error(`Oops! Unknown size passed to progress bar: ${size}`);
 
 	return (
 		<ProgressWrapper htmlFor='progress-bar'>
@@ -41,10 +47,11 @@ const ProgressWrapper = styled.label``;
 
 const Progress = styled.progress`
 	background-color: ${COLORS.transparentGray15};
-	border-radius: 4px;
+	border-radius: var(--borderRadius);
 	height: var(--height);
 	width: 370px;
 	padding: var(--padding);
+	box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 
 	&::-webkit-progress-bar {
 		background-color: transparent;
@@ -54,13 +61,13 @@ const Progress = styled.progress`
 		background-color: ${COLORS.primary};
 		border-top-left-radius: inherit;
 		border-bottom-left-radius: inherit;
-		border-radius: ${(p) => p.value > 99 && '4px'};
+		border-radius: ${(p) => p.value > 99 && 'var(--borderRadius)'};
 	}
 
 	&::-webkit-progress-value {
 		background-color: ${COLORS.primary};
-		border-top-left-radius: 4px;
-		border-bottom-left-radius: 4px;
-		border-radius: ${(p) => p.value > 99 && '4px'};
+		border-top-left-radius: var(--borderRadius);
+		border-bottom-left-radius: var(--borderRadius);
+		border-radius: ${(p) => p.value > 99 && 'var(--borderRadius)'};
 	}
 `;
